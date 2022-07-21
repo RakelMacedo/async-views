9dd50f126e5ed994efe4f665c82d5241d4cdfbad
Rakel <macedorakel35@gmail.com>
2022-07-21 00:36:35 -0300

    update views for the exercise

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -1,28 +1,17 @@
-import asyncio, httpx
+import asyncio
 from time import sleep
 from django.http import HttpResponse
 
 async def http_call_async():
-    for num in range(1,6):
-        await asyncio.sleep(1)
-        print(num)
-    async with httpx.AsyncClient() as client:
-        r = await client.get("https://httpbin.org")
-        print(r)
-
-def http_call_sync():
-    for num in range(1,6):
-        sleep(1)
-        print(num)
-        r = httpx.get("https://httpbin.org")
-        print(r)
+    print("Hello")
+    await asyncio.sleep(1)
+    print("World!")
+    await asyncio.sleep(1)
+    print("Finish")
 
 async def async_views(request):
     loop = asyncio.get_event_loop()
     loop.create_task(http_call_async())
-    return HttpResponse("Non-blocking HTTP request")
+    return HttpResponse("We are in asynchronous processing!")
 
-def sync_views(request):
-    http_call_sync()
-    return HttpResponse("Blocking HTTP request")
 
